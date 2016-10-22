8f3c4b99e0e7280ccd89c197f14b9e5ebfcd91ca
macbook <macbook@macbooks-MacBook-Air.local>
2016-10-22 18:01:16 +0530

    added changes

--- js/script.js
+++ js/script.js
@@ -15,3 +15,102 @@
 
 // 	})
 // })
+
+
+// $('#view_all').click(function(){
+
+// 	// $(".more-products-list").slideToggle();
+// 	$(".more-products-list").slideToggle( "slow", function() {});
+// 	$('#view_all').val("View Less");
+
+// })
+
+
+$('.more-products').click(function(){
+
+	$("#more-products").slideDown( "slow", function() {});
+
+})
+   /**
+     * EFECTO PARA FLECHAS EN ACORDEON
+     */
+    
+$(document).on('show','.accordion', function (e) {
+     //$('.accordion-heading i').toggleClass(' ');
+     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+});
+
+$(document).on('hide','.accordion', function (e) {
+    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+});
+
+
+// $('#close-more-product').click(function(){
+
+// 	$("#more-product").slideUp( "slow", function() {});
+
+// })
+
+// $(".thumbnails").hover(function(){
+
+//     alert("hello");
+// })
+
+function hovered(id,cat_id){
+
+    $("#"+cat_id).attr('src','img/p1.png');
+    // $("#"+cat_id).css('width','95%');
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
